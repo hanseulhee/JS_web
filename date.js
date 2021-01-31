33e8930a0e3863a2d798b893ff03bef8d54c91ea
hanseulhee <3021062@gmail.com>
2021-01-31 13:56:01 +0900

    date-js arrow function

--- date.js
+++ date.js
@@ -1,7 +1,7 @@
 const dateContainer = document.querySelector(".js-date");
 const dateTitle = dateContainer.querySelector("h2");
 
-function getDate() {
+const getDate = () => {
   const date = new Date();
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
@@ -9,10 +9,10 @@ function getDate() {
   dateTitle.innerText = `${year}년${month < 10 ? `0${month}` : month}월${
     day < 10 ? `0${day}` : day
   }일`;
-}
+};
 
 
-function init() {
+const init = () => {
   getDate();
-}
+};
 init();
